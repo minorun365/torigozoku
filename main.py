b2ccf5ca4c50f0b5341211286414307767fdbe00
mi-onda <mi-onda@kddi.com>
2025-06-05 08:50:29 +0900

    チャット入力の例文を具体的な内容に変更

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ def main():
     client, session_id, messages = initialize_session()
     display_chat_history(messages)
     
-    if prompt := st.chat_input("例：2025年5月の売り上げがよくない。アドバイスちょうだい！"):
+    if prompt := st.chat_input("例「先月の売上をもとに、経営改善アドバイスをちょうだい！」"):
         messages.append({"role": "human", "text": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
